20f1574f3a7a7885b34d6845acd2a72adc3e7c2b
Marianne Skolander <marianne.skolander@gmail.com>
2016-04-05 16:30:46 +0200

    start version

---
+++ webdevfinal/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using webdevfinal.ViewModels;
+
+
+namespace webdevfinal.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+    }
+}
---
+++ webdevfinal/ViewModels/ContactForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webdevfinal.ViewModels
+{
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+    }
+}
